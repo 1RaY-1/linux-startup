73bfb43468aeb01deea396175d8a0806c6c0f17e
1RaY-1 <78962948+1RaY-1@users.noreply.github.com>
2021-05-26 14:36:14 +0200

    test file

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# you can try to make this script startup to check if the program is actually working
+# you can try making this script startup to check if the program is actually working
 
 mkdir /home/$USER/Desktop/ITS-WORKING
