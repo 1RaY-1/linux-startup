456fe858a3b87f6336bfaa62d73c5deae3d1bed7
1RaY-1 <78962948+1RaY-1@users.noreply.github.com>
2021-06-03 11:54:03 +0200

    test file

--- test/test.sh
+++ test/test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # you can try making this script startup to check if the program is actually working
-# this script creates a directory named 'ITS-WORKING' in your desktop
+# this script creates a directory named 'ITS-WORKING' on your desktop
 
 mkdir /home/$USER/Desktop/ITS-WORKING
