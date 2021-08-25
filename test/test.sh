0082a2d8f78341056097bfb3d3e05b72e60c0910
1RaY-1 <78962948+1RaY-1@users.noreply.github.com>
2021-08-25 17:53:15 +0200

    Update test.sh

--- test/test.sh
+++ test/test.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-# you can try making this script startup to check if the program is actually working
+# you can try making this script startup to check if the program is actually working for you
 # this script creates a directory named 'ITS-WORKING' on your desktop
 
 mkdir /home/$USER/Desktop/ITS-WORKING
 
-# if not working, then use this:
+# if not working, then try this (but uncomment this lines below and remove line 6):
 #u=YOUR_USERNAME_HERE # <--ENTER YOUR USERNAME
 #mkdir /home/$u/Desktop/ITS-WORKING
