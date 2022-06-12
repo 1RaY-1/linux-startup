87bd0f4d034f79e0e4c3ab8f97ea49a616c7d582
1RaY-1 <78962948+1RaY-1@users.noreply.github.com>
2022-06-12 11:32:48 +0200

    improve installer

--- install.sh
+++ install.sh
@@ -24,50 +24,46 @@ fi
 
 
 check(){
-
     if [ $EUID -ne 0 ]; then
         echo -e "Please run me as root\nType: sudo bash $0"
         exit 1
     fi
 
     if [ ! -f ${needed_file} ]; then
-        echo "Hey! Did you remove '${needed_file}' ??"
+        echo "Missing needed file: '${needed_file}'"
         exit 1
     fi
 }
 
-ask_if_ok(){
-    echo "
-I will:
-1: Copy '${needed_file}' to '${needed_dir}' as '${needed_file%.*}'
-2: Make it executable
-
-So you will be able to run '${needed_file}' from any directory."
-    printf "\nIs this ok?[y/n]\n"
-    read is_ok
-
-    case $is_ok in
-        y | yes | Y | YES) : ;;
-        *) exit 0 ;;
-    esac
-}
 
 do_it(){
+    # just in case
     if [ -f "${needed_dir}${needed_file%.*}" ]; then
-        echo -e "Wow, file '${needed_dir}${needed_file%.*}' already exists.\nExiting... "
+        echo -e "[!] File '${needed_dir}${needed_file%.*}' already exists."
+        echo "Wanna replace it or cancel this installation? [1:replace/2:cancel]"
+        read choice
+        if [[ $choice = "replace" || $choice -eq 1 ]]; then rm ${needed_dir}${needed_file%.*}
+        else echo "Exiting...";exit 0
+        fi
     fi
+    echo "Installing..."
+    sleep 0.5
 
     cp ${needed_file} ${needed_dir}${needed_file%.*}
     chmod +x ${needed_dir}${needed_file%.*}
 
     echo "
 Done!
-Now you can easily execute this script by typing: 'sudo ${needed_file%.*}' from any directory
+Now you can run this program by typing: 'sudo ${needed_file%.*}'
+
+To test if it works, you can try making '/test/test.py' startup.
+
 To remove it, type: sudo rm ${needed_dir}${needed_file%.*}
+
+For more info read README.md 
 "
 }
 
-check_for_problems
-ask_if_ok
+check
 do_it
 exit
